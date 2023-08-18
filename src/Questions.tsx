72496e92bb527d9e2f959524572a87ac762b88e6
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2023-08-18 17:34:04 -0300

    feat: adicionando componente UI (Pill) e esboçando tela de Q&A

--- src/Questions.tsx
+++ src/Questions.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Pill from "./ui/Pill";
 import Wrapper from "./ui/Wrapper";
 function Questions() {
 
     return (
         <Wrapper width='972px' height='505px' padding='4.3%'>
-            aaabb
+            <Pill isQuestion text="pergunta"/>
+            <Pill text="resposta 1"/>
+            <Pill text="resposta 2"/>
+            <Pill text="resposta 3"/>
         </Wrapper>
     )
 }
---
+++ src/ui/Pill.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Pill.css";
+
+function Pill({text, isQuestion}: {text: string, isQuestion: boolean}) {
+
+    return (
+        <div className={`pill-container ${isQuestion ? 'question' : ''}`}>
+            <span>{text}</span>
+        </div>
+    )
+}
+
+export default Pill;
